f438c97d2e64e170786ac91598b9bca1fd0d2c77
dainabase <admin@dainamics.ch>
2025-08-14 07:41:41 +0200

    feat: Add Notification Center component exports

---
+++ packages/ui/src/components/notification-center/index.tsx
@@ -0,0 +1,12 @@
+export { 
+  NotificationCenter,
+  type NotificationCenterProps,
+  type Notification,
+  type NotificationType,
+  type NotificationPriority,
+  type NotificationStatus,
+  type NotificationAction,
+  type NotificationMeta,
+  type NotificationGroup,
+  type NotificationPreferences
+} from './notification-center';

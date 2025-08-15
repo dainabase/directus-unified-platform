0a4c79695fbec115788339ac12b806e30611ef0c
dainabase <admin@dainamics.ch>
2025-08-15 11:53:20 +0200

    feat: Create forms-bundle for lazy loading

---
+++ packages/ui/src/components/forms-bundle.ts
@@ -0,0 +1,34 @@
+// Forms Bundle - Lazy loaded forms components
+// These components are loaded on-demand to reduce initial bundle size
+
+export { Button } from './button';
+export { Input } from './input';
+export { Label } from './label';
+export { Textarea } from './textarea';
+export { Select } from './select';
+export { Checkbox } from './checkbox';
+export { RadioGroup } from './radio-group';
+export { Switch } from './switch';
+export { Slider } from './slider';
+export { Toggle } from './toggle';
+export { ToggleGroup } from './toggle-group';
+export { Form } from './form';
+export { FormsDemo } from './forms-demo';
+export { DatePicker } from './date-picker';
+export { DateRangePicker } from './date-range-picker';
+export { ColorPicker } from './color-picker';
+export { FileUpload } from './file-upload';
+export { Rating } from './rating';
+
+// Export types
+export type { ButtonProps } from './button';
+export type { InputProps } from './input';
+export type { LabelProps } from './label';
+export type { TextareaProps } from './textarea';
+export type { SelectProps } from './select';
+export type { CheckboxProps } from './checkbox';
+export type { RadioGroupProps } from './radio-group';
+export type { SwitchProps } from './switch';
+export type { SliderProps } from './slider';
+export type { ToggleProps } from './toggle';
+export type { ToggleGroupProps } from './toggle-group';

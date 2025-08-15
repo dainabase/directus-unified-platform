3bb822fee097a1879d7f702cd5914d0e9a820420
dainabase <admin@dainamics.ch>
2025-08-15 22:10:51 +0200

    fix: Correct import path for utils in timeline component

--- packages/ui/src/components/timeline/timeline.tsx
+++ packages/ui/src/components/timeline/timeline.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Check, Circle, Clock, AlertCircle, X } from 'lucide-react';
-import { cn } from '@/lib/utils';
+import { cn } from '../../lib/utils';
 
 export interface TimelineItem {
   id: string;

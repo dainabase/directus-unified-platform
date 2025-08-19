48789545d023b7292fb71bdf2af8aca9fe35d89b
dainabase <admin@dainamics.ch>
2025-08-19 22:26:24 +0200

    feat(showcase): Add PostCSS configuration for Tailwind CSS processing

---
+++ packages/ui/showcase/postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: {
+    'tailwindcss': {},
+    'autoprefixer': {},
+    'postcss-preset-env': {
+      stage: 3,
+      features: {
+        'nesting-rules': true,
+        'custom-properties': true,
+        'custom-media-queries': true,
+      },
+    },
+  },
+}

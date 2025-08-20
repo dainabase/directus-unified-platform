556d2673e879887372b1d7e1b11dbf7bbb7af097
dainabase <admin@dainamics.ch>
2025-08-20 12:18:37 +0200

    fix: Add .tsx extension to image-cropper imports in index.ts

--- packages/ui/src/components/image-cropper/index.ts
+++ packages/ui/src/components/image-cropper/index.ts
@@ -1,4 +1,4 @@
 // ImageCropper Component Export
-export { ImageCropper } from '../image-cropper';
-export type { ImageCropperProps } from '../image-cropper';
-export { default } from '../image-cropper';
+export { ImageCropper } from '../image-cropper.tsx';
+export type { ImageCropperProps } from '../image-cropper.tsx';
+export { default } from '../image-cropper.tsx';

11773543b83362a76eada84bd2353fd8d5c8b41a
dainabase <admin@dainamics.ch>
2025-08-20 07:41:23 +0200

    fix(showcase): Simplify PostCSS config to remove postcss-preset-env dependency

--- packages/ui/showcase/postcss.config.js
+++ packages/ui/showcase/postcss.config.js
@@ -2,13 +2,5 @@ export default {
   plugins: {
     'tailwindcss': {},
     'autoprefixer': {},
-    'postcss-preset-env': {
-      stage: 3,
-      features: {
-        'nesting-rules': true,
-        'custom-properties': true,
-        'custom-media-queries': true,
-      },
-    },
   },
 }

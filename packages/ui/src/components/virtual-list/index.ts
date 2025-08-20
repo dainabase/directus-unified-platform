ef2544d14e4aec5023965d340c6e6ffa45156e73
dainabase <admin@dainamics.ch>
2025-08-20 12:23:26 +0200

    fix: Remove .tsx extension from virtual-list imports

--- packages/ui/src/components/virtual-list/index.ts
+++ packages/ui/src/components/virtual-list/index.ts
@@ -1,4 +1,4 @@
 // VirtualList Component Export
-export { VirtualList } from '../virtual-list.tsx';
-export type { VirtualListProps } from '../virtual-list.tsx';
-export { default } from '../virtual-list.tsx';
+export { VirtualList } from '../virtual-list';
+export type { VirtualListProps } from '../virtual-list';
+export { default } from '../virtual-list';
